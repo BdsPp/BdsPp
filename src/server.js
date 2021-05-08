d28a0d4c51ab655f544a09233299bd8227005d78
360tetsu360 <66517224+360tetsu360@users.noreply.github.com>
2021-05-08 21:50:53 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const dgram = require("dgram");
 const { EventEmitter } = require("events");
 const Log = require("./logger");
+const Util = require("./util");
 const Packet = require("./packets/packet");
 class Server extends EventEmitter {
     constructor(Host, Port, Logger = require("./logger")) {
@@ -49,11 +50,12 @@ class Server extends EventEmitter {
 
 function OnPacket(server, packet, remote) {
     if (packet.ID == 0x01) {
-        var ServerIDStr = "MCPE;a;390;-5;0;10;13253860892328930865;Bedrock level;Survival;1;19132;19133;";
-        var out = new Buffer.from([0x1c, 0x00, 0x00, 0x00, 0x00, 0x04, 0x70, 0x59, 0xe3, 0xb0, 0x6d, 0x1b, 0x33, 0x51, 0x09, 0x2d,
-            0x5b, 0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56,
-            0x78, 0x00, ServerIDStr.length,
-            ...(new TextEncoder).encode(ServerIDStr)
+        var ServerIDStr = 'MCPE;testMOTD;431;1.16.221;0;10;12712847230353616219;Bedrock level;Survival;1;19132;19133;';
+        var IDstrBuf = Util.StrtoBuf(ServerIDStr);
+        const out = new Buffer.from([
+            0x1c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xb0, 0x6d, 0x1b, 0x33, 0x51, 0x09, 0x2d,  
+            0x5b, 0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56,  
+            0x78,...Util.NumtoBuf(IDstrBuf.length,2),...IDstrBuf
         ]);
         server.send(out, remote.port, remote.address, (err) => {});
     }
