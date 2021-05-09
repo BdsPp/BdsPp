24d6fa8cd909c1655b9ef0dbc9d00bce014fdab2
360tetsu360 <tetsuhiko.game.minecraft@gmail.com>
2021-05-09 19:53:51 +0900

    update!!!

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ class Server extends EventEmitter {
             //this.HandleMany(remote.address);
             var bytes = buffer.toJSON().data;
             var packet = new Packet(bytes, this, remote);
-            OnPacket(this, packet, remote, _Handler);
+            OnPacket(packet, _Handler);
             this.emit("onPacket", packet, remote);
         });
     }
@@ -78,21 +78,8 @@ class Server extends EventEmitter {
     }
 }
 
-function OnPacket(server, packet, remote, handler) {
+function OnPacket(packet,handler) {
     handler.handle(packet);
-    /*if (packet.Data[0] == 0x01) {
-        var ServerIDStr = 'MCPE;testMOTD;431;1.16.221;0;10;12712847230353616219;Bedrock level;Survival;1;19132;19133;';
-        var IDstrBuf = Util.StrtoBuf(ServerIDStr);
-        const out = new Buffer.from([
-            0x1c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xb0, 0x6d, 0x1b, 0x33, 0x51, 0x09, 0x2d,
-            0x5b, 0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56,
-            0x78, ...Util.NumtoBuf(IDstrBuf.length, 2), ...IDstrBuf
-        ]);
-        server.send(out, remote.port, remote.address, (err) => {});
-    } else {
-        handler.handle(packet);
-        //console.log(handler);
-    }*/
 }
 
 module.exports = Server;
