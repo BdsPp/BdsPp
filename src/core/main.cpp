fe79b28053d41cc441754d194f5d974e21d7d166
360tetsu360 <tetsuhiko.game.minecraft@gmail.com>
2021-05-09 10:42:00 +0900

    napi

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,1 +1,22 @@
-#include <stdio.h>
+#include <napi.h>
+#include <vector>
+
+namespace core {
+
+    Napi::Value PacketMethod(const Napi::CallbackInfo& info)
+    {
+        Napi::Env env = info.Env();
+
+        return env.Null();
+    }
+
+    Napi::Object Initialize(Napi::Env env, Napi::Object exports)
+    {
+        exports.Set(Napi::String::New(env, "packet"),
+            Napi::Function::New(env, PacketMethod));
+        return exports;
+    }
+
+    NODE_API_MODULE(NODE_GYP_MODULE_NAME, Initialize)
+
+}
--- src/server.js
+++ src/server.js
@@ -8,22 +8,22 @@ const { existsSync, readdirSync, lstatSync } = require('fs');
 class Server extends EventEmitter {
     constructor(Host, Port, Logger = require("./logger"), PluginPath = join(__dirname, '..', 'plugins')) {
         super();
-        this.HandleMany = function(ip){
-            if(!this.reqs) this.reqs = {};
-            if(this.reqs[ip]){
-                if(this.reqs[ip].rateLimited) return new Packet(0x15, 'Too Many Packets.(' + this.reqs[ip].value + ')');
+        this.HandleMany = function (ip) {
+            if (!this.reqs) this.reqs = {};
+            if (this.reqs[ip]) {
+                if (this.reqs[ip].rateLimited) return new Packet(0x15, 'Too Many Packets.(' + this.reqs[ip].value + ')');
                 this.reqs[ip].value += 1;
-                if(this.reqs[ip].value <= 100) {
+                if (this.reqs[ip].value <= 100) {
                     this.reqs[ip].rateLimited = true;
                     setTimeout(() => {
                         this.reqs[ip].rateLimited = false;
-                    },1000);
+                    }, 1000);
                 };
-            }else{
+            } else {
                 this.reqs[ip] = { value: 1, rateLimited: false };
                 setTimeout(() => {
                     this.reqs[ip] = { value: 0, rateLimited: false };
-                },1000);
+                }, 1000);
             };
         };
         this.PluginPath = PluginPath;
@@ -37,7 +37,7 @@ class Server extends EventEmitter {
         this.Server.on('listening', () => {
             this.emit('listening', this.Host, this.Port);
         });
-        this.Server.on('message', function(buffer, remote) {
+        this.Server.on('message', function (buffer, remote) {
             this.HandleMany(remote.address);
             var bytes = buffer.toJSON().data;
             var packet = new Packet(bytes[0], bytes.slice(1));
@@ -51,15 +51,15 @@ class Server extends EventEmitter {
     Stop() {
         this.Server.close();
     }
-    LoadPlugins(){
-        if(!existsSync(this.PluginPath)) return this.Logger.Error('Plugin Not Found\n');
+    LoadPlugins() {
+        if (!existsSync(this.PluginPath)) return this.Logger.Error('Plugin Not Found\n');
         const plugins = readdirSync(this.PluginPath);
         let i = 0;
         plugins.forEach(plugin => {
-            if(!lstatSync(this.PluginPath + '/' + plugin).isDirectory()) return;
+            if (!lstatSync(this.PluginPath + '/' + plugin).isDirectory()) return;
             const files = readdirSync(this.PluginPath + '/' + plugin);
-            if(!files.includes('index.js')) return;
-            if(files.includes('disable')) return;
+            if (!files.includes('index.js')) return;
+            if (files.includes('disable')) return;
             const file = require(this.PluginPath + '/' + plugin + '/index.js');
             file.Handler(this.Handler);
             this.Logger.Info('Loaded ' + plugin + ' [' + (file.Version || '1.0.0') + ']\n');
@@ -74,14 +74,12 @@ function OnPacket(server, packet, remote) {
         var ServerIDStr = 'MCPE;testMOTD;431;1.16.221;0;10;12712847230353616219;Bedrock level;Survival;1;19132;19133;';
         var IDstrBuf = Util.StrtoBuf(ServerIDStr);
         const out = new Buffer.from([
-            0x1c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xb0, 0x6d, 0x1b, 0x33, 0x51, 0x09, 0x2d,  
-            0x5b, 0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56,  
-            0x78,...Util.NumtoBuf(IDstrBuf.length,2),...IDstrBuf
+            0x1c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xb0, 0x6d, 0x1b, 0x33, 0x51, 0x09, 0x2d,
+            0x5b, 0x00, 0xff, 0xff, 0x00, 0xfe, 0xfe, 0xfe, 0xfe, 0xfd, 0xfd, 0xfd, 0xfd, 0x12, 0x34, 0x56,
+            0x78, ...Util.NumtoBuf(IDstrBuf.length, 2), ...IDstrBuf
         ]);
-        server.send(out, remote.port, remote.address, (err) => {});
+        server.send(out, remote.port, remote.address, (err) => { });
     }
 }
 
 module.exports = Server;
-// unnntiiiiiwwww
-//pushdekirukana
