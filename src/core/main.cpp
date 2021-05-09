a90fafdc2258356cf5d1c26881e0309dd3ef3d91
360tetsu360 <66517224+360tetsu360@users.noreply.github.com>
2021-05-09 11:45:16 +0900

    Update main.cpp

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,13 +1,18 @@
 #include <napi.h>
 #include <vector>
 
+
 namespace core {
 
     Napi::Value PacketMethod(const Napi::CallbackInfo& info)
     {
         Napi::Env env = info.Env();
+        Napi::Array Packet = info[0].As<Napi::Array>();
 
-        return env.Null();
+        int elem = 0;
+        Napi::Value val = Packet[elem];
+        int f = val.As<Napi::Number>().Int32Value();
+        return Napi::Number::New(env, f);
     }
 
     Napi::Object Initialize(Napi::Env env, Napi::Object exports)
