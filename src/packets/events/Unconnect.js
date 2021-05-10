a03161fc135dc35ff3923b2296ac790ba4d11aca
360tetsu360 <tetsuhiko.game.minecraft@gmail.com>
2021-05-10 17:34:51 +0900

    guid

--- src/packets/events/Unconnect.js
+++ src/packets/events/Unconnect.js
@@ -1,9 +1,10 @@
 module.exports = function (packet, Util) {
     var ServerIDStr = `MCPE;testMOTD;431;1.16.221;0;10;${packet.GUID};Bedrock level;Survival;1;19132;19133;`;
     var IDstrBuf = Util.StrtoBuf(ServerIDStr);
+    const GUIDbuf = Buffer.allocUnsafe(8);
+    GUIDbuf.writeBigUInt64LE(BigInt(packet.GUID), 0);
     const out = new Buffer.from([
-        0x1c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xb0, 0x6d, 0x1b, 0x33, 0x51, 0x09, 0x2d,
-        0x5b, ...packet.MagicCode, ...Util.NumtoBuf(IDstrBuf.length, 2), ...IDstrBuf
+        0x1c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, ...GUIDbuf, ...packet.MagicCode, ...Util.NumtoBuf(IDstrBuf.length, 2), ...IDstrBuf
     ]);
     packet.Server.send(out, packet.Remote.port, packet.Remote.address, (err) => { });
 };
