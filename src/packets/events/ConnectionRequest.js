e3c83aa0d9667c02a24dd0a381de3da44b1d4633
360tetsu360 <tetsuhiko.game.minecraft@gmail.com>
2021-05-10 18:52:13 +0900

    connectionrequest 1 and 2

---
+++ src/packets/events/ConnectionRequest.js
@@ -0,0 +1,20 @@
+module.exports = function (packet, Util) {
+    if (packet.Data[0] == 0x5) {
+        console.log("1");
+        const GUIDbuf = Buffer.allocUnsafe(8);
+        GUIDbuf.writeBigUInt64LE(BigInt(packet.GUID), 0);
+        const out = Buffer.from([0x06, ...packet.MagicCode, ...GUIDbuf, 0x0, 0x05, 0x78]);
+        packet.Server.send(out, packet.Remote.port, packet.Remote.address, (err) => { });
+    }
+    else if (packet.Data[0] == 0x7) {
+        console.log("2");
+        const GUIDbuf = Buffer.allocUnsafe(8);
+        GUIDbuf.writeBigUInt64LE(BigInt(packet.GUID), 0);
+        var ip = packet.Remote.address.split('.').map(str => 0xFF - parseInt(str, 10));
+        const out = Buffer.from([0x08, ...packet.MagicCode, ...GUIDbuf, 0x4, ...ip, ...Util.NumtoBuf(packet.Remote.port, 2), 0x05, 0x78,0x00]);
+        packet.Server.send(out, packet.Remote.port, packet.Remote.address, (err) => { });
+    }
+    else if (packet.Data[0] == 0x84) {
+        console.log("a");
+    };
+};
--- src/packets/packetHandler.js
+++ src/packets/packetHandler.js
@@ -13,7 +13,18 @@ class PacketHandler extends PluginAPI {
         if (packet.Data[0] == 0x01) {
             events.Unconnect(packet);
             this.fire('UnConnect', packet.Remote);
-        };
+        }
+        else if (packet.Data[0] == 0x05 || packet.Data[0] == 0x07) {
+            events.ConnectionRequest(packet);
+            this.fire('ConnectionRequest', packet.Remote, (packet.Data[0] - 3) /2);
+        }
+        else if (packet.Data[0] == 0x84) {
+            if (packet.Data[10] == 0x9) {
+                events.ConnectionRequest(packet);
+                this.fire('ConnectionRequest', packet.Remote, 3);
+            }
+        }
     }
+
 };
 module.exports = PacketHandler;
