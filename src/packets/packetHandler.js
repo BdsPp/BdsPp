844b306f437534bb8240078f66060ee62fc1e027
FurenjiCH <furenjich@gmail.com>
2021-05-10 12:07:39 +0900

    commit

--- src/packets/packetHandler.js
+++ src/packets/packetHandler.js
@@ -1,11 +1,14 @@
 const events = require('./events');
 const PluginAPI = require("../PluginAPI/api");
+const { EventEmitter } = require('../PluginAPI/api');
 class PacketHandler extends PluginAPI {
     constructor() {
         super();
+        this.Packet = new EventEmitter();
         return this;
     }
     handle(packet) {
+        this.Packet.emit(packet.Data[0], packet);
         if (packet.Data[0] == 0x01) {
             events.Unconnect(packet);
             this.fire('UnConnect', packet.Remote);
