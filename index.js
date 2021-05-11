21485449e1ee41946e53f2d1ba81c577b37ede1a
Sueqkjs <sueqk@outlook.jp>
2021-05-11 21:44:39 +0900

    typescript?

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const Server = require("./src/server");
-const server_1 = new Server("localhost", 19132);
-server_1.LoadPlugins();
+const server = new Server("localhost", 19132);
+server.LoadPlugins();
 var Log = require("./src/logger");
-server_1.on('listening', function() {
-    Log.Info("Port: " + server_1.Options.Port + "\n");
-    Log.Info("IP: " + server_1.Options.Host + "\n");
+server.on('listening', function() {
+    Log.Info("Port: " + server.Options.Port + "\n");
+    Log.Info("IP: " + server.Options.Host + "\n");
     Log.Info("The server has been started!\n");
     process.on('SIGINT', () => {
         Log.Info("Server Stoping..");
         process.exit(128);
     });
 });
-server_1.Start();
+server.Start();
