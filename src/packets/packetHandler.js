e1c4c9a81ab5f3c341dfa4e5905137b69280f203
360tetsu360 <tetsuhiko.game.minecraft@gmail.com>
2021-05-11 19:55:43 +0900

    pornhub

--- src/packets/packetHandler.js
+++ src/packets/packetHandler.js
@@ -10,31 +10,32 @@ class PacketHandler extends PluginAPI {
     handle(packet) {
         this.Packet.emit(packet.Data[0], packet);
         this.Packet.emit("*", packet);
-        if (packet.Data[0] == 0x01) {
-            events.Unconnect(packet);
-            this.fire('UnConnect', packet.Remote);
-        }
-        else if (packet.Data[0] == 0x05 || packet.Data[0] == 0x07) {
-            events.ConnectionRequest(packet);
-            this.fire('ConnectionRequest', packet.Remote, (packet.Data[0] - 3) /2);
-        }
-        else if (packet.Data[0] == 0xc0) {
-            console.log(packet.Data[3]);
-            if (packet.Data[3] == 0) {
+
+        switch (packet.ID) {
+            case 0x01:
+                events.Unconnect(packet);
+                this.fire('UnConnect', packet.Remote);
+                break;
+            case 0x05:
                 events.ConnectionRequest(packet);
-                this.fire('ConnectionRequest', packet.Remote, 3);
-            }
-            //console.log(packet.Data.length)
-        }
-        else if (packet.Data[0] == 0x84) {
-            if (packet.Data[10] == 0x9) {
+                this.fire('ConnectionRequest', packet.Remote);
+                break;
+            case 0x07:
                 events.ConnectionRequest(packet);
-                this.fire('ConnectionRequest', packet.Remote, 3);
-            }
-            else if (packet.Data[14] == 0x13) {
+                this.fire('ConnectionRequest', packet.Remote);
+                break;
+            case 0x09:
                 events.ConnectionRequest(packet);
-                this.fire('ConnectionRequest', packet.Remote, 3);
-            }
+                this.fire('ConnectionRequest', packet.Remote);
+                break;
+            case 0x013:
+                events.ConnectionRequest(packet);
+                this.fire('ConnectionRequest', packet.Remote);
+                break;
+            case 0xc0:
+                events.ConnectionRequest(packet);
+                this.fire('ConnectionRequest', packet.Remote);
+                break;
         }
     }
 
